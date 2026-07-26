4b8d81be821549fd067c555964c3cf9921a8dce7
Guillermo R. <guillermo.rodriguezz2510@gmail.com>
2026-07-26 12:31:38 -0600

    cambios en base de datos

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@prisma/config'
+import 'dotenv/config'
+
+export default defineConfig({
+  earlyAccess: true,
+  studio: { port: 5555 },
+  datasource: {
+    url: process.env.POSTGRES_PRISMA_URL,
+  },
+  migrations: {
+    url: process.env.POSTGRES_URL_NON_POOLING,
+  }
+})
